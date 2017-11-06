9ea646d71ce92bd68a652f19e9a58ce1e0662524
afeez <afeezhammed@gmail.com>
2017-11-06 08:11:46 +0100

    Añadidos al login nombre apellido

--- server/boot/root.js
+++ server/boot/root.js
@@ -18,5 +18,10 @@ module.exports = function(app) {
     });
   });
 
+ //verified
+  app.get('/verified', function(req, res) {
+    res.render('verified');
+  });
+
   
   };
--- server/datasources.local.js
+++ server/datasources.local.js
@@ -24,7 +24,7 @@ module.exports = {
       port: 465,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWOED
+        pass: process.env.EMAIL_PASSWORD
       }
     }]
   }
