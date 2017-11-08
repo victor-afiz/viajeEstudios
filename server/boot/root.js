63feb54c4d3229af8ffe39e395dc70e174ed7c2b
afeez <afeezhammed@gmail.com>
2017-11-08 10:00:11 +0100

    Hasta aqui ya funciona la registracion y  el login

--- server/boot/root.js
+++ server/boot/root.js
@@ -5,6 +5,7 @@ var dsConfig = require('../datasources.local.js');
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
+  var User = server.models.Usuario;
   //login page
   router.get('/', function(req, res) {
     var credentials = dsConfig.myEmailDataSource.transports[0].auth;
@@ -19,5 +20,37 @@ module.exports = function(server) {
     res.render('verified');
 });
 
-
+//log a user in
+  router.post('/login', function(req, res) {
+    User.login({
+      email: req.body.email,
+      password: req.body.password
+    }, 'user', function(err, token) {
+      if (err) {
+        if(err.details && err.code === 'LOGIN_FAILED_EMAIL_NOT_VERIFIED'){
+          res.render('reponseToTriggerEmail', {
+            title: 'Login failed',
+            content: err,
+            redirectToEmail: '/api/users/'+ err.details.userId + '/verify',
+            redirectTo: '/',
+            redirectToLinkText: 'Click here',
+            userId: err.details.userId
+          });
+        } else {
+          res.render('response', {
+            title: 'Login failed. Wrong username or password',
+            content: err,
+            redirectTo: '/',
+            redirectToLinkText: 'Please login again',
+          });
+        }
+        return;
+      }
+      res.render('home', {
+        email: req.body.email,
+        accessToken: token.id,
+        redirectUrl: '/api/users/change-password?access_token=' + token.id
+      });
+    });
+});
 };
